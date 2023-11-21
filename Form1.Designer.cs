b7dcb6ebd48bd178cd31b634c701480c3ee931cc
marinaoleinik <marina.oleinik@tthk.ee>
2023-11-21 12:24:48 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Tooded_TARpv22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kat_Box = new System.Windows.Forms.ComboBox();
+            this.kat_lbl = new System.Windows.Forms.Label();
+            this.Lisa_Kat_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Toode_txt = new System.Windows.Forms.TextBox();
+            this.Kogus_txt = new System.Windows.Forms.TextBox();
+            this.Hind_txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(102, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(686, 137);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Kat_Box
+            // 
+            this.Kat_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kat_Box.FormattingEnabled = true;
+            this.Kat_Box.Location = new System.Drawing.Point(145, 202);
+            this.Kat_Box.Name = "Kat_Box";
+            this.Kat_Box.Size = new System.Drawing.Size(185, 33);
+            this.Kat_Box.TabIndex = 1;
+            // 
+            // kat_lbl
+            // 
+            this.kat_lbl.AutoSize = true;
+            this.kat_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.kat_lbl.Location = new System.Drawing.Point(4, 206);
+            this.kat_lbl.Name = "kat_lbl";
+            this.kat_lbl.Size = new System.Drawing.Size(135, 26);
+            this.kat_lbl.TabIndex = 2;
+            this.kat_lbl.Text = "Kategooriad:";
+            // 
+            // Lisa_Kat_btn
+            // 
+            this.Lisa_Kat_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Lisa_Kat_btn.Location = new System.Drawing.Point(336, 202);
+            this.Lisa_Kat_btn.Name = "Lisa_Kat_btn";
+            this.Lisa_Kat_btn.Size = new System.Drawing.Size(119, 33);
+            this.Lisa_Kat_btn.TabIndex = 3;
+            this.Lisa_Kat_btn.Text = "Lisa kategooriat";
+            this.Lisa_Kat_btn.UseVisualStyleBackColor = true;
+            this.Lisa_Kat_btn.Click += new System.EventHandler(this.Lisa_Kat_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Toode:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(41, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kogus:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(58, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hind:";
+            // 
+            // Toode_txt
+            // 
+            this.Toode_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Toode_txt.Location = new System.Drawing.Point(145, 23);
+            this.Toode_txt.Name = "Toode_txt";
+            this.Toode_txt.Size = new System.Drawing.Size(180, 32);
+            this.Toode_txt.TabIndex = 7;
+            // 
+            // Kogus_txt
+            // 
+            this.Kogus_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kogus_txt.Location = new System.Drawing.Point(145, 88);
+            this.Kogus_txt.Name = "Kogus_txt";
+            this.Kogus_txt.Size = new System.Drawing.Size(180, 32);
+            this.Kogus_txt.TabIndex = 8;
+            // 
+            // Hind_txt
+            // 
+            this.Hind_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Hind_txt.Location = new System.Drawing.Point(145, 141);
+            this.Hind_txt.Name = "Hind_txt";
+            this.Hind_txt.Size = new System.Drawing.Size(180, 32);
+            this.Hind_txt.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button1.Location = new System.Drawing.Point(145, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Lisa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button2.Location = new System.Drawing.Point(226, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Uuenda";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button3.Location = new System.Drawing.Point(307, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Kustuta";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Hind_txt);
+            this.Controls.Add(this.Kogus_txt);
+            this.Controls.Add(this.Toode_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lisa_Kat_btn);
+            this.Controls.Add(this.kat_lbl);
+            this.Controls.Add(this.Kat_Box);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox Kat_Box;
+        private System.Windows.Forms.Label kat_lbl;
+        private System.Windows.Forms.Button Lisa_Kat_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Toode_txt;
+        private System.Windows.Forms.TextBox Kogus_txt;
+        private System.Windows.Forms.TextBox Hind_txt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tooded_TARpv22
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect=new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\Tooded_DB.mdf;Integrated Security=True");
+
+        SqlDataAdapter adapter_toode, adapter_kategooria;
+        SqlCommand command;
+        public Form1()
+        {
+            InitializeComponent();
+            NaitaAndmed();
+            NaitaKategooriad();
+        }
+
+        public void NaitaKategooriad()
+        {
+            connect.Open();
+            adapter_kategooria = new SqlDataAdapter("SELECT Kategooria_nimetus FROM Kategooriatabel", connect);
+            DataTable dt_kat = new DataTable();
+            adapter_kategooria.Fill(dt_kat);
+            foreach (DataRow item in dt_kat.Rows)
+            {
+                Kat_Box.Items.Add(item["Kategooria_nimetus"]);
+            }
+            connect.Close();
+        }
+
+        private void Lisa_Kat_btn_Click(object sender, EventArgs e)
+        {
+            command = new SqlCommand("INSERT INTO Kategooriatabel (Kategooria_nimetus) VALUES (@kat)",connect);
+            connect.Open();
+            command.Parameters.AddWithValue("@kat", Kat_Box.Text);
+            command.ExecuteNonQuery();
+            connect.Close();
+            Kat_Box.Items.Clear();
+            NaitaKategooriad();
+        }
+
+
+        public void NaitaAndmed()
+        {
+            connect.Open();
+
+            DataTable dt_toode = new DataTable();
+            adapter_toode = new SqlDataAdapter("SELECT * FROM Toodetabel", connect);
+            adapter_toode.Fill(dt_toode);
+            dataGridView1.DataSource = dt_toode;
+
+            connect.Close();
+        }
+    }
+}
